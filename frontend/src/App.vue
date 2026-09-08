8950e199199b6eeac3d35ba838e39a5ecdb45bf8
lbl <1791778603@qq.com>
2026-09-08 23:34:48 +0800

    style: soften light theme colors

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,7 @@ const { isLoggedIn } = useAuthStore()
 </script>
 
 <template>
-  <div class="min-h-screen bg-slate-50 dark:bg-slate-900">
+  <div class="min-h-screen bg-slate-100 dark:bg-slate-900">
     <template v-if="isLoggedIn">
       <div class="flex min-h-screen">
         <AppSidebar />
